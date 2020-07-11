e656c10c14c1d3fc9154febe25ca705c5500fb49
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2020-07-11 23:33:34 +0300

    framework commenterd, garbage collected

--- Assets/Authoring/Scripts/EntityAuthoring/IAuthoringComponent.cs
+++ Assets/Authoring/Scripts/EntityAuthoring/IAuthoringComponent.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 namespace Sc2Simulation.Authoring
 {
+    /// <summary>
+    /// Если мы хотим сделать какой-либо свой кастомный компонент (который не трансформация и не рендеринг),
+    /// над надо наследоваться от этого класса, чтобы и добавлять его на авторскую энтитю. А также обеспечить
+    /// конвертацию в промежуточную версию (ComponentConvertion), из которой потом десериализуется уже 
+    /// рантаймовый компонент.
+    /// </summary>
     public abstract class AuthoringComponent : MonoBehaviour
     {
         public virtual SerializedComponentInfo ConvertToRuntime(EntityAuthoring[] entitiesAuthoring) => new SerializedComponentInfo(GetType().Name.Substring(0, GetType().Name.Length - "Authoring".Length) + "Conversion", JsonUtility.ToJson(this));
